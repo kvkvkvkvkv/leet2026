995f376de4ece518c57066992873fb0680e2e0af
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-02 09:01:07 +0530

    Time: 4 ms (99.81%) | Memory: 47.4 MB (40.64%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -4,22 +4,38 @@ class Solution {
         for(int i=0;i<grid.length;i++) {
             for(int j=0;j<grid[0].length;j++) {
                 if(grid[i][j]==1) {
-                    sum+=peri(grid,i,j);
+                    int count = 4;
+                    if(i-1>=0 && grid[i-1][j]==1) {
+                        count--;
+                    }
+
+                    if(i+1<grid.length && grid[i+1][j]==1) {
+                        count--;
+                    }
+
+                    if(j-1>=0 && grid[i][j-1]==1) {
+                        count--;
+                    }
+
+                    if(j+1<grid[0].length && grid[i][j+1]==1) {
+                        count--;
+                    }
+                    sum += count;
                 }
             }
         }
         return sum;
     }
 
-    public int peri(int[][] grid, int r, int c) {
-        if(r<0 || c<0 || r==grid.length || c==grid[0].length || grid[r][c]==0) {
-            return 1;
-        }
-        if(grid[r][c]==2) {
-            return 0;
-        }
-        grid[r][c] = 2;
-        return peri(grid, r+1, c) + peri(grid, r-1, c) + peri(grid, r, c+1) +
-        peri(grid, r, c-1);
-    }
+    // public int peri(int[][] grid, int r, int c) {
+    //     if(r<0 || c<0 || r==grid.length || c==grid[0].length || grid[r][c]==0) {
+    //         return 1;
+    //     }
+    //     if(grid[r][c]==2) {
+    //         return 0;
+    //     }
+    //     grid[r][c] = 2;
+    //     return peri(grid, r+1, c) + peri(grid, r-1, c) + peri(grid, r, c+1) +
+    //     peri(grid, r, c-1);
+    // }
 }
