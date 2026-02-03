cfbdbc1d29759737c151b3c1ad39654d205701ae
nihilistpenguin <33504944+kvkvkvkvkv@users.noreply.github.com>
2026-02-03 08:12:56 +0530

    Time: 8 ms (64.57%) | Memory: 50.2 MB (73.68%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[][] direction = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public void wallsAndGates(int[][] rooms) {
+        Queue<int[]> gates = new LinkedList<>();
+        for(int i=0;i<rooms.length;i++) {
+            for(int j=0;j<rooms[0].length;j++) {
+                if(rooms[i][j] == 0) {
+                    gates.add(new int[]{i,j,0});
+                }
+            }
+        }
+
+        while (!gates.isEmpty()) {
+            int[] currentGate = gates.poll();
+            for(int[] dir:direction) {
+                int r = dir[0]+currentGate[0];
+                int c = dir[1]+currentGate[1];
+
+                if(r>=0 && c>=0 && r<rooms.length && c<rooms[0].length
+                && 1+currentGate[2]<rooms[r][c]) {
+                    rooms[r][c] = 1 + currentGate[2];
+                    gates.add(new int[]{r,c,rooms[r][c]});
+                }
+            }
+        }
+    }
+}
